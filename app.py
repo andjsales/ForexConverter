17d599ff0e7bc4efe854d09dca8eaf56765d49b7
andrew.sales <andjsales@gmail.com>
2023-10-16 18:28:03 -0500

    organized functions in app.py, added docstrings

--- app.py
+++ app.py
@@ -6,48 +6,81 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret'
 
 base_url = 'http://api.exchangerate.host/'
-os.environ['API_KEY_ENV_VAR'] = 'daff0b9b4d855f63c2ad3a313e0425d8'
 api_key = os.getenv("API_KEY_ENV_VAR")
 
-response = requests.get(
-    f"http://api.exchangerate.host/list?access_key={api_key}")
-if response.status_code == 200:
+
+def get_supported_currencies(api_key):
+    """
+    Display currencies supported by API
+    """
+
+    response = requests.get(f"{base_url}list?access_key={api_key}")
     data = response.json()
-    supported_currencies = data.get('currencies', {})
-else:
-    supported_currencies = {}
+    return data.get('currencies', {})
+
+
+def convert_currency(convert_from, convert_to, amount):
+    """
+    Conversion of currency
+    """
+
+    response = requests.get(
+        f"{base_url}convert?from={convert_from}&to={convert_to}&amount={amount}&access_key={api_key}")
+    data = response.json()
+    result = data.get('result')
+
+    if result is not None:
+        return round(float(result), 2)
+    else:
+        flash_error_message(response)
+        return None
+
+
+def flash_error_message(response):
+    """
+    Flash error if status_code receives an error.
+    """
+
+    error_data = response.json().get('error', {})
+    error_code = error_data.get('code', 0)
+    convert_from = request.form['convert_from'].upper()
+    convert_to = request.form['convert_to'].upper()
+    amount = request.form['amount']
+    if error_code == 400:
+        flash("Bad Request")
+    elif error_code == 401:
+        flash(f"Not a valid currency: '{convert_from}'")
+    elif error_code == 402:
+        flash(f"Not a valid currency: '{convert_to}'")
+    elif error_code == 403:
+        flash(f"Not a valid number: '{amount}'")
+    elif error_code == 404:
+        flash("Not Found")
+    else:
+        flash("An error occurred")
 
 
 @app.route('/', methods=['GET', 'POST'])
 def homepage():
-    """display the currenvy converter"""
+    """
+    Displays currency
+    """
 
     converted_amount = None
     result_message = None
+    supported_currencies = get_supported_currencies(api_key)
 
     if request.method == 'POST':
         convert_from = request.form['convert_from'].upper()
         convert_to = request.form['convert_to'].upper()
         amount = request.form['amount']
 
-        response = requests.get(
-            f"{base_url}convert?from={convert_from}&to={convert_to}&amount={amount}&access_key={api_key}")
-        data = response.json()
-        error_data = response.json().get('error', {})
-        error_code = error_data.get('code', 0)
+        converted_amount = convert_currency(convert_from, convert_to, amount)
 
-        if error_code == 401:
-            flash(f"Not a valid currency: {convert_from}")
-
-        elif error_code == 402:
-            flash(f"Not a valid currency: {convert_to}")
-
-        elif error_code == 403:
-            flash(f"Not a valid number: {amount}")
-
-        else:
-            converted_amount = round(float(data.get('result', 'N/A')), 2)
+        if converted_amount is not None:
             result_message = f"{amount} {convert_from} is equivalent to {converted_amount} {convert_to}."
+        else:
+            result_message = "Conversion failed:"
 
     return render_template('index.html', converted_amount=converted_amount, result_message=result_message, supported_currencies=supported_currencies)
 
